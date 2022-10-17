ee6b3872facba2cf7bbf3215cbc394c82f73379e
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2022-10-17 15:28:14 +0330

    adding onload calculation fn and bmi state

--- script/app.js
+++ script/app.js
@@ -11,15 +11,19 @@ const heightValuePrint = $.querySelector("#height-val");
 heightValuePrint.innerHTML = height.value;
 // Result and catergory
 const resultElm = $.querySelector("#result");
-const catergory=$.querySelector('#category')
-
+const catergory = $.querySelector("#category");
 // functions
 function onLoadValues() {
   weight.value = 20;
   weightValuePrint.innerHTML = `${weight.value} kg`;
   height.value = 100;
   heightValuePrint.innerHTML = `${height.value} cm`;
+  let heightCmToMeter = height.value / 100;
+  let calculateBmiOnLoad = weight.value / (heightCmToMeter * heightCmToMeter);
+  let roundedBmi = calculateBmiOnLoad.toPrecision(3);
+  bmiStatus(roundedBmi);
 }
+// getting weight and height and calculate BMI
 function bmiCalculator() {
   // changing the value of value printers
   heightValuePrint.innerHTML = `${height.value} cm`;
@@ -27,11 +31,32 @@ function bmiCalculator() {
   let heightCmToMeter = height.value / 100;
   //   calculate the bmi
   let calculateBmi = weight.value / (heightCmToMeter * heightCmToMeter);
+  let roundedBmi = calculateBmi.toPrecision(3);
   //   changing title of result elm
-  let newResultTitle= resultElm.innerHTML=Math.round(calculateBmi)
+  bmiStatus(roundedBmi);
+}
+// Chossing the status of category
+function bmiStatus(bmi) {
+  //   conditions of Bmi
+  resultElm.innerHTML = bmi;
+  if (bmi < 18.5) {
+    resultElm.style.color = "rgb(255,196,77)";
+    catergory.innerHTML = "Under weight";
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    catergory.innerHTML = "Normal Weight";
+    resultElm.style.color = "rgb(11,232,129)";
+  }
+  if (bmi >= 25 && bmi <= 29.9) {
+    catergory.innerHTML = "Obese";
+    resultElm.style.color = "rgb(255,94,87)";
+  }
+  if (bmi > 29.9) {
+    catergory.innerHTML = "Over Weight";
+    resultElm.style.color = "red";
+  }
 }
 // Event Listeners
-
 window.addEventListener("load", onLoadValues);
 weight.addEventListener("change", bmiCalculator);
 height.addEventListener("change", bmiCalculator);
